e879896ee98a382f78b3d9c28d1cc611bb85c24c
shtmkr <privet.noobs@mail.ru>
2019-11-24 20:06:58 +0300

    added: hw 3095

--- 3095/server.js
+++ 3095/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 const webserver = express();
+const path = require('path');
+const fs = require('fs');
+const logFN = path.join(__dirname, '_server.txt');
 
 const variants = require('./variants');
 const port = 8480;
@@ -31,6 +34,10 @@ webserver.post('/vote', (req, res) => {
     stats[update.code] = +stats[update.code] + 1;
     console.log(stats[update.code] );
     res.send({[update.code] : stats[update.code], id: update.id});
+
+    const logFd = fs.openSync(logFN, 'w');
+    fs.writeSync(logFd, JSON.stringify(stats));
+    fs.closeSync(logFd);
 });
 
 const createHtml = () => {
@@ -38,8 +45,10 @@ const createHtml = () => {
         return `<input type="button" id="${variant.id}" value="${variant.text}" onclick="vote(this.id)">`;
     }).join('');
     let viewStats = '';
-    for (let p in stats) {
-        viewStats += `<li class="li">${p}: ${stats[p]}</li>`
+    let read = readFileSync(logFN);
+    let parsed = JSON.parse(read);
+    for (let p in parsed) {
+        viewStats += `<li class="li">${p}: ${parsed[p]}</li>`
     }
     return `<div>
             <h2>Варианты ответов</h2>
@@ -58,6 +67,33 @@ const createHtml = () => {
             })
      }</script>`
      };
+
+function readFileSync(filePath)
+{
+    var fd = fs.openSync(filePath, 'r');
+
+    var content = '';
+
+    var buffer = new Buffer(10);
+
+    buffer.fill(0);
+
+    var readCount = fs.readSync(fd, buffer, null, 10);
+
+    while(readCount > 0)
+    {
+        console.log("Read " + readCount + " bytes.");
+
+        content += buffer.toString().substr(0, readCount);
+
+        readCount = fs.readSync(fd, buffer, null, 10);
+    }
+
+    fs.closeSync(fd);
+
+    console.log("File content : " + content);
+    return content;
+}
 
 
 webserver.listen(port,()=>{
