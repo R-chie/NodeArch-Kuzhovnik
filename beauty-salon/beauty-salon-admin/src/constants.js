bfcfe1421598cc5f263e995d77eabcdbb9d5e2fb
R-Chie <artem.kuzhovnik@gmail.com>
2020-01-12 23:59:11 +0300

    port 8480 server

--- beauty-salon/beauty-salon-admin/src/constants.js
+++ beauty-salon/beauty-salon-admin/src/constants.js
@@ -1,5 +1,5 @@
 const constants = {
-    HOST_URL: 'http://localhost:3000',
+    HOST_URL: 'http://178.172.195.18:8480',
     /*HOST_URL: 'https://posassist.3c.by',*/
     MAIN_APP_ROUTE: '/admin',
 };
